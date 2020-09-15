d6deae0adb7d08ff2865280c95b95f61637838c3
Hai Dang Nguyen Pham <hai.nguyen-pham@uni-konstanz.de>
2020-09-15 18:04:10 +0200

    update 15.09.20

--- StateSpace_Model/test_solver_for_ideal_nlp.m
+++ StateSpace_Model/test_solver_for_ideal_nlp.m
@@ -9,8 +9,8 @@ import casadi.*
 
 %% get input and output data
 sr = 100; % sampling rate
-N_cyc = 3;
-N_amp = 3;
+n_cyc = 2;
+n_stimuli = 2;
 
 % Turn noise test on=1 or off else
 noise_test = 1;
@@ -20,14 +20,14 @@ if noise_test==1
      
 elseif noise_test==0
     noise_amp = 1;
-    Q = cell(N_amp,N_cyc,6); 
+    Q = cell(n_stimuli,n_cyc,6); 
 end
 
-amp = 1:N_amp;%[1,2,4,8]
+amp = 1:n_stimuli;%[1,2,4,8]
 %[x,xi]=pseudogen3(2,[1 1],[0 2],25);
 [x,xi]=pseudogen3(3,[1 0 -1],[1 0 2],25);
 %[x,xi]=pseudogen3(5,[0 0 1 -1 -1],[2 0 1 0 2],25);
-ndata = N_amp*size(x,2);
+ndata = n_stimuli*size(x,2);
 np = 8;
 %% reduction of data points
 
@@ -61,22 +61,22 @@ params_Start = [0.55; 0.25; 0.3; 1; 0.3*mp*9.81*H/180*pi; 0.08; 0.1; 20];
 
 
 if noise_test == 1
-    for i_noise=1:noise_amp
-        u = zeros(N_amp,size(xi,2)*N_cyc);
+    for n_noise=1:noise_amp
+        u = zeros(n_stimuli,size(xi,2)*n_cyc);
 
         % simulate N_cyc cycles
-        for k=1:N_amp
-            u(k,:) = repmat( amp(1,k)*( xi/(max(xi)-min(xi)) ),1,N_cyc);
+        for k=1:n_stimuli
+            u(k,:) = repmat( amp(1,k)*( xi/(max(xi)-min(xi)) ),1,n_cyc);
         end
 
         u = u(:,1:NSamp:end,1);
-        Q{i_noise,1} = u; % Save stimulus
+        Q{n_noise,1} = u; % Save stimulus
         N = size(u,2)-1;
         
         % evaluate F for every time step
 
         out = zeros(7,size(u,1),size(u,2));
-        for j=1:N_amp % number of different amplitudes
+        for j=1:n_stimuli % number of different amplitudes
             X = zeros(length(ode.x),1); % set all states to zero (start of new amplitude)
             for i=1:N % samples within an amplitude
                 x_out = F('x0',X,'p',[u(j,i); params_Start; zeros(2,1)]); 
@@ -87,20 +87,20 @@ if noise_test == 1
 
         % get first state trajectory (which is body sway)
         y(:,:) = out(1,:,:);
-        Q{i_noise,2} = y;   % Save body sway trajectory
+        Q{n_noise,2} = y;   % Save body sway trajectory
         
         % add sway variability to simulated sway response
-        noise = wgn(size(y,1),size(y,2),0); % white noise Signal
+        noise = wgn(1,size(y,2),0); % white noise Signal
         fnyq = NSamp/2;
         [b,a] = butter(2,fco/fnyq,'bandpass'); % bandpass filter
         noise = filter(b,a,noise);
-        
-        y = y + 10*i_noise*noise;
-
-        Q{i_noise,3} = y;   % Save body sway trajectory after including noise
+        %noise = repmat(noise,n_stimuli,1);
+        y = y + n_noise*noise;
+        Q{n_noise,7} = noise(1,:);
+        Q{n_noise,3} = y;   % Save body sway trajectory after including noise
         %% create nlp
         disp('Creating nlp')
-        [ nlp, m, lbw, ubw, lbg, ubg, w0, R ] = get_nlp(ode, u, y, out, N_amp, N, tt);
+        [ nlp, m, lbw, ubw, lbg, ubg, w0, R ] = get_nlp(ode, u, y, out, n_stimuli, N, tt);
 
         %% Create an NLP solver
         disp('Creating nlp solver')
@@ -122,7 +122,7 @@ if noise_test == 1
                     'lbg', lbg, 'ubg', ubg, 'p',m);
         toc
         sol.x = full(sol.x);
-        Q{i_noise,4}=sol.x(end-7:end,:);    % Save solution of the parameter
+        Q{n_noise,4}=sol.x(end-7:end,:);    % Save solution of the parameter
         params_opt = sol.x(end-np+1:end,:);
         ng = size(nlp.g,1);
         nx = size(nlp.x,1);
@@ -149,7 +149,7 @@ if noise_test == 1
         % Rank check for more information see documentary
         r_g = rank(Dg_full);
         r_r = rank([Dr_full; Dg_full]);
-        sprintf('Rank check with N_amp= %d and noise_amp= %d',N_amp,noise_amp)
+        sprintf('Rank check with N_amp= %d and noise_amp= %d',n_stimuli,noise_amp)
         if r_g ~= ng || r_r ~= nx
             sprintf('rank check failed : Rank g = %d and ng = %d; Rank r = %d and nx = %d',r_g,ng,r_r,nx)
         else
@@ -160,8 +160,8 @@ if noise_test == 1
         %conf_b = [params_opt-(1.96.*(sqrt(diag(C_p)./(ndata)))) params_opt+(1.96.*(sqrt(diag(C_p)./(ndata))))];
         conf_b = [params_opt-(2.*np/(ndata-np)).*1.96.*(sqrt(diag(C_p))) params_opt+(2.*np/(ndata-np)).*1.96.*(sqrt(diag(C_p)))]; 
         conf_err = 1.96.*(sqrt(diag(C_p)./(ndata)));
-        Q{i_noise,5} = conf_b;  % Save confident bounds
-        Q{i_noise,6} = conf_err;
+        Q{n_noise,5} = conf_b;  % Save confident bounds
+        Q{n_noise,6} = conf_err;
 
         clear y w0 conf_b conf_err
 
@@ -169,8 +169,8 @@ if noise_test == 1
 
 % If noise test is turned off
 elseif noise_test ==0    
-    for i_amp=1:N_amp
-        for j_cyc=1:N_cyc
+    for i_amp=1:n_stimuli
+        for j_cyc=1:n_cyc
             u = zeros(i_amp,size(xi,2)*j_cyc);
 
             % simulate N_cyc cycles
@@ -203,7 +203,7 @@ elseif noise_test ==0
             fnyq = NSamp/2;
             [b,a] = butter(2,fco/fnyq,'bandpass'); % bandpass filter
             noise = filter(b,a,noise);
-
+            %noise = repmat(noise,i_amp,1);
             y = y + noise_amp*noise;
 
             Q{i_amp,j_cyc,3} = y;   % Save body sway trajectory after including noise
@@ -270,7 +270,7 @@ elseif noise_test ==0
             %conf_b = [params_opt-(2.*np/(ndata-np)).*1.96.*(sqrt(diag(C_p))) params_opt+(2.*np/(ndata-np)).*1.96.*(sqrt(diag(C_p)))]; 
             conf_b = [params_opt-(1.96.*(sqrt(diag(C_p)./(ndata)))) params_opt+(1.96.*(sqrt(diag(C_p)./(ndata))))];
             conf_err = 1.96.*(sqrt(diag(C_p)./(ndata)));
-            Q{i_amp,j_cyc,5} = conf_b; % Save confidence bounds
+            Q{i_amp,j_cyc,5} = conf_b; % Save confident bounds
             Q{i_amp,j_cyc,6} = conf_err;
             clear y w0 conf_b conf_err
         end
@@ -278,17 +278,22 @@ elseif noise_test ==0
 end
 
 
+%% plot stimuli strength
 
 
+    
+%% Plot results testing noise amplitude
 if noise_test == 1
-    %% Plot results testing noise amplitude
 
     bound = reshape(cell2mat(Q(:,5)),np,noise_amp*2);
     bound_error = reshape(cell2mat(Q(:,6)),np,noise_amp);
     est_param = reshape(cell2mat(Q(:,4)),np,noise_amp);
+         
+
     p_names = ["K_{grav}"; "K_{th}"; "\lambda"; "K_{s}"; "K_{d}"; "\tau"; "K_{f}"; "\tau_f"];
-    figure(1)
-    for i=1:8
+    figure(1) 
+    set(gcf, 'Position',  [200, 200, 600, 800])  
+    for i=1:np
         subplot(4,2,i);    
         plot_real = plot(1:noise_amp,params_Start(i).*ones(noise_amp,1),'s','linewidth',2);
         plot_real.LineStyle = 'none';
@@ -304,100 +309,86 @@ if noise_test == 1
         plot_CI1.MarkerFaceColor = [1 .6 .6];    
         set(gca, 'XLim',[0 noise_amp+1],'XTick', 1:noise_amp)    
 
-        ylim= ([min(bound(i,:))-0.1*abs(min(bound(i,:))) max(bound(i,:))+0.2*abs(max(bound(i,:)))]);
+        %ylim= ([min(bound(i,:))-0.1*abs(min(bound(i,:))) max(bound(i,:))+0.2*abs(max(bound(i,:)))]);
 
         title(p_names(i) )
-        xlabel('Amplitude number')
+        xlabel('number of noise (n_{noise})')
         grid
+        matlab2tikz('5.tex')
     end
-elseif noise_test==0
-    %% Plot results testing duration and amplitude 
-    cyc_ind = 2;
-    bound = reshape(cell2mat(Q(:,cyc_ind,5)),np,N_amp*2);
-    bound_error = reshape(cell2mat(Q(:,cyc_ind,6)),np,N_amp);
-	est_param = reshape(cell2mat(Q(:,cyc_ind,4)),np,N_amp);
-    p_names = ["K_{grav}"; "K_{th}"; "\lambda"; "K_{s}"; "K_{d}"; "\tau"; "K_{f}"; "\tau_f"];
+    %%
+    noi = cell2mat(Q(1,7));
     figure(2)
-    for i=1:8
+    for i=1:n_noise
+        
+        plot(i.*noi(1,:));
+        hold on
+        xlabel('time (s)')
+        ylabel('tilt angle (°)')
+    end
+        legend('pp 1° (n_{noise}=1)','pp 2° (n_{noise}=1)','pp 3° (n_{noise}=3)')%,'pp 4° (n_{noise}=4)'
+%% Plot results testing duration and amplitude
+
+elseif noise_test==0
+    % Choose which cycle should be plotted
+    cyc_ind = 4;
+    
+    bound_error = reshape(cell2mat(Q(:,cyc_ind,6)),np,n_stimuli);
+    bound_error_old = bound_error;	
+    est_param = reshape(cell2mat(Q(:,cyc_ind,4)),np,n_stimuli);
+    % Scale plots by cutting errorbars which are 5 times larger then second
+    % element
+    index_cut = zeros(np,1);
+    for j= 1:np
+        if bound_error(j,1) > 5*bound_error(j,2)
+            index_cut(j) = 1;
+            %bound_error(j,1) = 2*bound_error(j,2);
+        end
+    end
+    
+    p_names = ["K_{grav}"; "K_{th}"; "\lambda"; "K_{s}"; "K_{d}"; "\tau"; "K_{f}"; "\tau_f"];
+    figure(1)        
+    set(gcf, 'Position',  [200, 200, 600, 800])    
+    for i=1:np
         subplot(4,2,i);    
-        plot_real = plot(1:N_amp,params_Start(i).*ones(N_amp,1),'s','linewidth',2);
+        plot_real = plot(1:n_stimuli,params_Start(i).*ones(n_stimuli,1),'s','linewidth',2);
         plot_real.LineStyle = 'none';
         plot_real.MarkerSize = 8;
         plot_real.MarkerEdgeColor = 'blue';
         plot_real.MarkerFaceColor = [0.2 .6 .6]; 
         hold on
-        plot_CI1 = errorbar(1:N_amp,est_param(i,:)',bound_error(i,:),'linewidth',1.5);
+        plot_CI1 = errorbar(1:n_stimuli,est_param(i,:)',bound_error(i,:),'linewidth',1.5);
         plot_CI1.LineStyle = 'none';
         plot_CI1.Marker = 'o';
         plot_CI1.MarkerSize = 5;
         plot_CI1.MarkerEdgeColor = 'red';
-        plot_CI1.MarkerFaceColor = [1 .6 .6];    
-        set(gca, 'XLim',[0 N_amp+1],'XTick', 1:N_amp)    
-        ylim= ([min(bound(i,:))-0.1*abs(min(bound(i,:))) max(bound(i,:))+0.2*abs(max(bound(i,:)))]);
+        plot_CI1.MarkerFaceColor = [1 .6 .6]; 
+    
+        if index_cut(i) ==1
+            set(gca, 'XLim',[0 n_stimuli+1],'XTick', 1:n_stimuli, 'YLim',[est_param(i,2)-1.3*bound_error(i,2) bound_error(i,2)+1.3*est_param(i,2)])
 
+        else
+            set(gca, 'XLim',[0 n_stimuli+1],'XTick', 1:n_stimuli, 'YLim',[min(est_param(i,:)-bound_error(i,:))-0.1*abs(min(est_param(i,:)-bound_error(i,:))) max(est_param(i,:)+bound_error(i,:))+0.2*abs(min(est_param(i,:)+bound_error(i,:)))])
+        end
         title(p_names(i) )
-        xlabel('Amplitude number')
+        xlabel('number of stimuli (n_{stimuli})')
         grid
+        %matlab2tikz('4.tex')
+    end
+    %%
+    stim = Q{n_stimuli,n_cyc,1};
+    figure(2)
+    for i = 1:n_stimuli
+        
+        plot(stim(i,:));
+        hold on
+        xlabel('time (s)')
+        ylabel('tilt angle (°)')
+        legend('pp 1°','pp 2°','pp 3°','pp 4°')
     end
 end
-%%
-% close all
-% figure(2)
-% Cycle index
-% cyc_ind = 2;
-% Parameter index
-% param_ind = 1;
-% 
-% real_param = params_Start(param_ind).*ones(N_amp,1);
-% bound = reshape(cell2mat(Q(:,cyc_ind,5)),np,N_amp*2);
-% est_param = reshape(cell2mat(Q(:,cyc_ind,4)),np,N_amp);
-% plot_CI = errorbar(1:N_amp,est_param(param_ind,:)',bound(param_ind,1:N_amp)',bound(param_ind,N_amp+1:end)');
-% 
-% plot_CI.LineStyle = 'none';
-% xticks([ 1 2 ])
-% xticklabels({'1','2'})
-% axis([0 N_amp+1 min(bound(param_ind,:))-1 max(bound(param_ind,:))+1])
-% plot_CI.Marker = 'o';
-% plot_CI.MarkerSize = 5;
-% plot_CI.MarkerEdgeColor = 'red';
-% plot_CI.MarkerFaceColor = [1 .6 .6];
-% title(['Confidence bounds for parameter ' ,num2str(param_ind), ' with ', num2str(cyc_ind),' cycles' ])
-% xlabel('Amplitude number')
-% grid on
-
-%%
-% Solution with k cycle and l amplitude
-% k=1;
-% l=1;
-% for j=2:k
-%     
-% figure(j)
-% plot(Q{l,j,2})
-% hold on
-% plot(Q{l,j,3})
-% legend('Simulated body sway','Body sway with noises', 'Location', 'Northeast') %,'Simulated'
-% title('Body sway trajectory')
-% xlabel('Time (s)')
-% ylabel('Body sway ({\circ})')
-% end
 
 
-
-% nu = 1;
-% nx = 7;
-% nb = 2;
-% s = nx+nu+nb;    
-% 
-% for i = N_amp:-1:1 
-%     u_in(i*(N+1)) = [];
-% end
-% %% Get Jacobian       
-% y_in = reshape(Q{2,1,3}',[(N+1)*N_amp,1]);
-% u_in = reshape(Q{2,1,4}',[(N+1)*N_amp,1]);
-% res_u = abs(u_opt - u_in);
-% res_y = abs(y_opt - y_in);
-% res_b1 = abs(b1_opt);
-% res_b7 = abs(b7_opt);
 
 % y_opt = [];
 % for i= 1:N_amp*N_cyc
@@ -415,4 +406,3 @@ end
 % for i= 1:N_amp*N_cyc
 %     b7_opt = [b7_opt;sol.x( 1+(i-1)*( N*s+(s-1) ) + nx +1:s: 1+(i-1)*( N*s+(s-1) ) + N*s + nx+1)];
 % end
-
